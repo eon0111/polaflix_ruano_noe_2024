986b30914025077f007b80d760bcf452720886c8
eon0111 <noe.reinosa@hotmail.com>
2024-03-08 18:29:06 +0100

    Creado proyecto SpringBoot

---
+++ src/main/java/localdomain/nruano/empresariales/polaflix_ruano_noe_2024/PolaflixRuanoNoe2024Application.java
@@ -0,0 +1,13 @@
+package localdomain.nruano.empresariales.polaflix_ruano_noe_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolaflixRuanoNoe2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolaflixRuanoNoe2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/localdomain/nruano/empresariales/polaflix_ruano_noe_2024/PolaflixRuanoNoe2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package localdomain.nruano.empresariales.polaflix_ruano_noe_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolaflixRuanoNoe2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
